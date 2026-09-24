4cf1a5b3c740ed7f8969ce6089b9e8396ccf2fab
arodriguez755-sudo <arodriguez755@uan.edu.co>
2026-09-24 10:34:31 -0500

    Create Diagrama 6.cpp

---
+++ Diagrama 6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+  int main() {
+      
+      float añona, añoac;
+      float edad;
+      
+      cout<< "Ingrese el año de nacimiento";
+      cin>>añona;
+      
+      cout<< "Ingrese el año actual";
+      cin>>añoac;
+      
+      edad = (añoac-añona);
+      
+      cout<<"La edad que tiene es:" <<edad;
+      
+      
+      
+      
+      
+      
+      
+      
+  }
